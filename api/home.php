c11098cd79b569a406078060e4a8a6c85e50f89b
mab1998 <mabrouk.bennaoui@gmail.com>
2020-12-28 18:39:40 +0100

    a

--- api/home.php
+++ api/home.php
@@ -4,8 +4,7 @@ require 'db.php';
 $data = json_decode(file_get_contents('php://input'), true);
  
 $uid = $data['uid'];
-echo strval($data);
-print_r($data);
+
 if($uid == '')
 {
 	$returnArr = array("ResponseCode"=>"401","Result"=>"false","ResponseMsg"=>"Something Went wrong  try again !");
